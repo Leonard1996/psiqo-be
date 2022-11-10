b485fbd7e2fc1c0c344d5d836b1a9361a1555ee3
Eri Dervishi <eridervishi12@gmail.com>
2022-11-11 00:09:05 +0100

    changes

--- chat/app.js
+++ chat/app.js
@@ -7,15 +7,14 @@ const http = require('http')
 const jwt = require('jsonwebtoken')
 const redis = require('redis')
 
-
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 const client = redis.createClient()
 
-client.on('error', (err) => console.log('Redis Client Error', err));
-(async function startServer() {
+client.on('error', (err) => console.log('Redis Client Error', err))
+;(async function startServer() {
   await client.connect()
 
   // await client.set('key', 'value');
@@ -29,86 +28,58 @@ client.on('error', (err) => console.log('Redis Client Error', err));
     },
   })
 
-
   io.on('connection', (socket) => {
-    console.log('New user connected')
+    // console.log('New user connected')
     const token = socket.handshake.headers.authorization
-   
-    socket.on('requestHistory', async function (receiver){
+
+    socket.on('requestHistory', async function (receiver) {
       const [result, error] = verifyJwtToken(token)
       if (error) return
-
-      const room = result.role === 'patient' ? `${result.id}-${receiver}` : `${receiver}-${result.id}`
-
+      const room = result.role === 'doctor' ? `${receiver}-${result.id}` : `${result.id}-${receiver}`
       const existingChat = await client.hGetAll(room)
-      console.log({existingChat})
       for (const value in existingChat) {
         existingChat[value] = JSON.parse(existingChat[value])
       }
-    
       io.sockets.in(room).emit('loadHistory', existingChat)
     })
 
     socket.on('create', async function (room) {
       const [result, error] = verifyJwtToken(token)
-      console.log("in create")
       if (error) return
       if (!room.split('-').includes(result.id.toString())) return
       socket.join(room)
-
       io.sockets.in(room).emit('roomCreated')
 
       socket.on('createMessage', async function (message) {
         const [result, error] = verifyJwtToken(token)
-        console.log("in create message")
         if (error) return
-         console.log("here")
         const date = Date.now()
-        message = {...message, date }
-        console.log({message})
-
+        message = { ...message, date }
         io.sockets.in(room).emit('newMessage', message)
-
         client.hSet(room, date, JSON.stringify(message))
         client.hSet(room, 'latest', JSON.stringify(message))
       })
 
-      socket.on('seen', async function(message){
+      socket.on('seen', async function (message) {
         const [result, error] = verifyJwtToken(token)
         if (error) return
-       
-        // const room = result.role === 'patient' ? `${result.id}-${message.seen.receiver}` : `${message.seen.receiver}-${result.id}`
-
         io.sockets.in(room).emit('notifySeen', message)
         client.hSet(room, message.date, JSON.stringify(message))
         client.hSet(room, 'latest', JSON.stringify(message))
-  
       })
 
-      socket.on('typing', async function(message){
+      socket.on('typing', async function (message) {
         const [result, error] = verifyJwtToken(token)
         if (error) return
-       
-        // const room = result.role === 'patient' ? `${result.id}-${message.receiver}` : `${message.receiver}-${result.id}`
-        io.sockets.in(room).emit('showTyping', {isTyping: result.id})
-  
+        io.sockets.in(room).emit('showTyping', { isTyping: result.id })
       })
 
-      socket.on('stopTyping', async function(message){
+      socket.on('stopTyping', async function (message) {
         const [result, error] = verifyJwtToken(token)
         if (error) return
-     
-       
-        // const room = result.role === 'patient' ? `${result.id}-${message.receiver}` : `${message.receiver}-${result.id}`
-
-        io.sockets.in(room).emit('unshowTyping', {isTyping: result.id})
-  
+        io.sockets.in(room).emit('unshowTyping', { isTyping: result.id })
       })
-
-  
     })
-
-  
 
     socket.on('disconnect', () => {
       console.log('disconnected from user')
@@ -121,14 +92,14 @@ client.on('error', (err) => console.log('Redis Client Error', err));
     console.log(`Chat server listening on port ${PORT} at ${new Date().toISOString().slice(0, 19).replace('T', ' ')}`)
   })
 
-  app.get("/history", async (request, response ) => {
-   response.send(await client.hGet(request.query.room, 'latest')) 
+  app.get('/history', async (request, response) => {
+    response.send(await client.hGet(request.query.room, 'latest'))
   })
 
-  app.get("/test", async (request, response ) => {
-    await client.set('test', 'it works');
-    response.send(await client.get('test')) 
-   })
+  app.get('/test', async (request, response) => {
+    await client.set('test', 'it works')
+    response.send(await client.get('test'))
+  })
 
   function verifyJwtToken(token) {
     try {
@@ -139,5 +110,4 @@ client.on('error', (err) => console.log('Redis Client Error', err));
       return [null, err]
     }
   }
-
 })()
